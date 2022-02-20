b636fbef07ef42d8c92a9e7956d917f430007a50
JoaoPdSsilva <joaopedro123jp1@outlook.com>
2022-02-19 23:14:59 -0300

    Pronto tudo certinho

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext.dispatcher import run_async
 import requests
 import re
-
-
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
     return url
 
-def bop(bot, update):
-    url = get_url()
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+@run_async
+def bop(update, context):
+    url = get_image_url()
     chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    context.bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater('5173115110:AAHJf6WEaMCevbNrbtoOrAB7AUtNOaFlEj4')
+    updater = Updater('5173115110:AAHJf6WEaMCevbNrbtoOrAB7AUtNOaFlEj4', use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop', bop))
+    dp.add_handler(CommandHandler('bop',bop))
     updater.start_polling()
     updater.idle()
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
